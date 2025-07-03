2e676b51657f4cf8e9a6fe8ca9f28d2b8c3a7357
brian <briankim31415@gmail.com>
2025-07-03 15:41:02 -0500

    batch files

---
+++ sbatch/geolife_basic.bat
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH -J inf_gan_geolife_basic
+#SBATCH -o run_logs/geolife_basic/job.o%j
+#SBATCH -e run_logs/geolife_basic/job.e%j
+#SBATCH -p gh
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -t 24:00:00
+#SBATCH --mail-type=all
+#SBATCH --mail-user=briankim31415@gmail.com
+#SBATCH -A CCR25007
+
+cd ..
+source ./venv/bin/activate
+module load gcc cuda python3
+python3 -m src.run --use_wandb --online --cfg_name=geolife_basic
